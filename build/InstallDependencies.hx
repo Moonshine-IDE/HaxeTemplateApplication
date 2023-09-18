64731ad0071e2def2b8e2be33c55c0efd5251cf5
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2023-09-18 11:21:13 +0200

    Simplify build script

---
+++ build/InstallDependencies.hx
@@ -0,0 +1,15 @@
+package build;
+
+class InstallDependencies {
+    public static function main() {        
+        Sys.command("haxelib --global update haxelib --quiet");
+
+        Sys.command("haxelib install hxcpp --quiet");
+        Sys.command("haxelib install lime --quiet");
+        Sys.command("haxelib install openfl --quiet");
+        Sys.command("haxelib install actuate --quiet");
+        Sys.command("haxelib install feathersui --quiet");
+        Sys.command("haxelib install amfio --quiet");
+        Sys.command("haxelib git champaign https://github.com/Moonshine-IDE/Champaign.git --quiet");
+    }
+}
